baa53f6ec3584eeced2115c62c556653472816c5
Andrei Grini <andy.grini@gmail.com>
2025-08-20 08:04:26 +0100

    added arrow icons

---
+++ src/icons/IconResize.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function IconResize(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 16 16" {...props}><path fill="currentColor" d="M7 0h2v16H7zM3 5L0 8l3 3V9h3V7H3zm13 3l-3-3v2h-3v2h3v2z"></path></svg>);
+}
