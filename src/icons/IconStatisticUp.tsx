5e08d2733e40327c355da061569e63b898e3abeb
Andrei Grini <andy.grini@gmail.com>
2025-08-30 20:03:34 +0100

    added icons statistic, sprinter

---
+++ src/icons/IconStatisticUp.tsx
@@ -0,0 +1,13 @@
+import type { SVGProps } from 'react'
+
+export function IconStatisticUp(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
+                <path strokeMiterlimit={5.759} d="M3 3v16a2 2 0 0 0 2 2h16"></path>
+                <path strokeMiterlimit={5.759} d="m7 14l4-4l4 4l6-6"></path>
+                <path d="M18 8h3v3"></path>
+            </g>
+        </svg>
+    )
+}
