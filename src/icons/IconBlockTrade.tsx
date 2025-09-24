b368bf4c3f9ac6f1e6655d2037ad50ae65d11219
Andrei Grini <andy.grini@gmail.com>
2025-09-24 18:49:05 +0100

    added new icons

---
+++ src/icons/IconBlockTrade.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from 'react'
+
+export function IconBlockTrade(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={48} height={48} viewBox="0 0 48 48" {...props}>
+            <path fill="#3f51b5" d="M42 37H6V25l10-15l14 7L42 6z"></path>
+            <path fill="#00bcd4" d="M42 42H6V32l10-8l14 2l12-9z"></path>
+        </svg>
+    )
+}
