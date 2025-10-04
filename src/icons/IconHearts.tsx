5c3bec9fe8ea2377535a0d8b8bd05b8404430ebf
Andrei Grini <andy.grini@gmail.com>
2025-10-04 11:49:35 +0100

    new icon update v1.0.27

--- src/icons/IconHearts.tsx
+++ src/icons/IconHearts.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { SVGProps } from 'react'
 
-export function IconHears(props: SVGProps<SVGSVGElement>) {
+export function IconHearts(props: SVGProps<SVGSVGElement>) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
             <g fill="none">
