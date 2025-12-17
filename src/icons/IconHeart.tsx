ed6c1c04d20be82cba39a2637e87d2b0d8d80af7
Andrei Grini <andy.grini@gmail.com>
2025-12-17 20:59:05 +0000

    new icon update v1.0.38

--- src/icons/IconHeart.tsx
+++ src/icons/IconHeart.tsx
@@ -2,21 +2,43 @@ import type { SVGProps } from 'react'
 
 export function IconHeart(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
-            <path fill='currentColor' fillOpacity={0} d='M3.5 11L12 20V7L7 5.5L3.5 7V11Z'>
-                <animate fill='freeze' attributeName='fill-opacity' begin='0.5s' dur='0.5s' values='0;1'></animate>
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path fill="currentColor" fillOpacity={0} d="M3.5 11L12 20V7L7 5.5L3.5 7V11Z">
+                <animate fill="freeze" attributeName="fill-opacity" begin="0.5s" dur="0.5s" values="0;1"></animate>
             </path>
             <path
-                fill='none'
-                stroke='currentColor'
+                fill="none"
+                stroke="currentColor"
                 strokeDasharray={30}
                 strokeDashoffset={30}
-                strokeLinecap='round'
+                strokeLinecap="round"
                 strokeWidth={2}
-                d='M12 8C12 8 12 8 12.7578 7C13.6343 5.84335 14.9398 5 16.5 5C18.9853 5 21 7.01472 21 9.5C21 10.4251 20.7209 11.285 20.2422 12C19.435 13.206 12 21 12 21M12 8C12 8 12 8 11.2422 7C10.3657 5.84335 9.06021 5 7.5 5C5.01472 5 3 7.01472 3 9.5C3 10.4251 3.27914 11.285 3.75777 12C4.56504 13.206 12 21 12 21'
+                d="M12 8C12 8 12 8 12.7578 7C13.6343 5.84335 14.9398 5 16.5 5C18.9853 5 21 7.01472 21 9.5C21 10.4251 20.7209 11.285 20.2422 12C19.435 13.206 12 21 12 21M12 8C12 8 12 8 11.2422 7C10.3657 5.84335 9.06021 5 7.5 5C5.01472 5 3 7.01472 3 9.5C3 10.4251 3.27914 11.285 3.75777 12C4.56504 13.206 12 21 12 21"
             >
-                <animate fill='freeze' attributeName='stroke-dashoffset' dur='0.5s' values='30;0'></animate>
+                <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.5s" values="30;0"></animate>
             </path>
+        </svg>
+    )
+}
+
+export function IconHeartMaterial(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"
+            ></path>
+        </svg>
+    )
+}
+
+export function IconHeartOffMaterial(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="M1 4.27L2.28 3L20 20.72L18.73 22l-3.55-3.56l-1.73 1.59L12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5c0-.95.23-1.83.63-2.6zM7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 2.57-1.58 4.82-4.21 7.47L5.27 3.45C5.95 3.16 6.7 3 7.5 3"
+            ></path>
         </svg>
     )
 }
---
+++ src/icons/IconWarning.tsx
@@ -0,0 +1,23 @@
+import type { SVGProps } from 'react'
+
+export function IconWarningOff(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="M12 18q.425 0 .713-.288T13 17t-.288-.712T12 16t-.712.288T11 17t.288.713T12 18m-1-4q0 .425.288.713T12 15t.713-.288T13 14v-1.025l-2-2zM9.925 7.1q-.25-.25-.3-.587t.125-.638L11.125 3.5q.15-.275.375-.388T12 3t.513.112t.362.388l7.1 12.25q.225.375-.125.65t-.675-.05zm9.85 15.475L18.175 21H2.725q-.575 0-.85-.5t0-1L8.35 8.325l6.975 6.95h-2.85L1.375 4.2q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3l18.375 18.4q.3.3.3.7t-.3.7t-.712.3t-.713-.3"
+            ></path>
+        </svg>
+    )
+}
+
+export function IconWarning(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="M2.725 21q-.275 0-.5-.137t-.35-.363t-.137-.488t.137-.512l9.25-16q.15-.25.388-.375T12 3t.488.125t.387.375l9.25 16q.15.25.138.513t-.138.487t-.35.363t-.5.137zM12 18q.425 0 .713-.288T13 17t-.288-.712T12 16t-.712.288T11 17t.288.713T12 18m0-3q.425 0 .713-.288T13 14v-3q0-.425-.288-.712T12 10t-.712.288T11 11v3q0 .425.288.713T12 15"
+            ></path>
+        </svg>
+    )
+}
