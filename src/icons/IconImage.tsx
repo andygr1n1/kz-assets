45e137d721eb869956fc7bfa9e06138f32f7c1ab
andygr1n1 <andy.grini@gmail.com>
2026-02-21 00:34:51 +0200

    new icon update v1.0.51

--- src/icons/IconImage.tsx
+++ src/icons/IconImage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { SVGProps } from 'react'
 
-export function BiImageAlt(props: SVGProps<SVGSVGElement>) {
+export function IconImage(props: SVGProps<SVGSVGElement>) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 16 16" {...props}>
             <path
